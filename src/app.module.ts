3557df10ea57a8cedcbbde2c6b4106003d79804c
Влад Варламов <duraroma4@gmail.com>
2025-01-31 16:32:21 +0200

    add app c

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { FiltersModule } from './filters/filters.module';
 import { CartModule } from './cart/cart.module';
 import { PaymentModule } from './payment/payment.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -19,5 +20,6 @@ import { PaymentModule } from './payment/payment.module';
     CartModule,
     PaymentModule,
   ],
+  controllers: [AppController]
 })
 export class AppModule { }
